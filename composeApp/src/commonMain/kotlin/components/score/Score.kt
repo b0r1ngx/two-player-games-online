717c6e937d62e74d1621b0576cebdb27d19787b5
Ivanov Kirill A <perkeboring@gmail.com>
2024-07-31 13:28:23 +0300

    correct Score position on Game screen

--- composeApp/src/commonMain/kotlin/components/score/Score.kt
+++ composeApp/src/commonMain/kotlin/components/score/Score.kt
@@ -1,12 +1,18 @@
 package components.score
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Score(
@@ -15,13 +21,23 @@ fun Score(
 ) {
     val p1score by remember { scoreViewModel.p1score }
     val p2score by remember { scoreViewModel.p2score }
-    Box {
-        // todo: add background
+    val rectColor = MaterialTheme.colorScheme.primary
+
+    Box(modifier = modifier.fillMaxSize()) {
         Text(
-            text = "$p2score : $p1score",
-            modifier = modifier,
-            textAlign = TextAlign.Center
+            "$p2score : $p1score",
+            modifier = Modifier
+                .rotate(270f)
+                .align(alignment = Alignment.CenterStart)
+                .drawBehind {
+                    // todo: stylize background
+                    drawRect(
+                        color = rectColor,
+                    )
+                },
+            // todo: stylize text
+            fontSize = 24.sp,
+            style = TextStyle(color = MaterialTheme.colorScheme.onPrimary)
         )
-        // todo: stylize text
     }
 }
